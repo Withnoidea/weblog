9fd5a6525886576eb5e3bde5cd8d3a50dd898936
Withnoidea <2482454653@qq.com>
2026-04-21 09:41:00 +0800

    add logback dependency & test log

--- weblog-springboot/weblog-web/src/test/java/com/quanxiaoha/weblog/web/WeblogWebApplicationTests.java
+++ weblog-springboot/weblog-web/src/test/java/com/quanxiaoha/weblog/web/WeblogWebApplicationTests.java
@@ -1,13 +1,26 @@
 package com.quanxiaoha.weblog.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class WeblogWebApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void testLog() {
+        log.info("info log");
+        log.debug("debug log");
+        log.error("error log");
+        log.warn("warn log");
+
+        // 占位符
+        String author = "犬小哈";
+        log.info("这是一行带有占位符日志，作者：{}", author);
+    }
 }
