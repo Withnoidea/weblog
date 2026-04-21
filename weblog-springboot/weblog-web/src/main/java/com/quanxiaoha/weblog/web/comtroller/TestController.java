31601d5387fbd85b0301defc018d88e97197ef51
Withnoidea <2482454653@qq.com>
2026-04-21 13:27:30 +0800

    参数校验

--- weblog-springboot/weblog-web/src/main/java/com/quanxiaoha/weblog/web/comtroller/TestController.java
+++ weblog-springboot/weblog-web/src/main/java/com/quanxiaoha/weblog/web/comtroller/TestController.java
@@ -3,9 +3,15 @@ package com.quanxiaoha.weblog.web.comtroller;
 import com.quanxiaoha.weblog.common.aspect.ApiOperationLog;
 import com.quanxiaoha.weblog.web.model.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
 
 /**
  * @Author: Withnoidea
@@ -16,9 +22,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class TestController {
+
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
-    public User test(@RequestBody User user) {
-        return user;
+    public ResponseEntity<String> test(@RequestBody @Validated User user, BindingResult bindingResult) {
+        // 是否存在校验错误
+        if (bindingResult.hasErrors()) {
+            // 获取校验不通过字段的提示信息
+            String errorMsg = bindingResult.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .collect(Collectors.joining(", "));
+
+            return ResponseEntity.badRequest().body(errorMsg);
+        }
+
+        // 返参
+        return ResponseEntity.ok("参数没有任何问题");
     }
+
 }
