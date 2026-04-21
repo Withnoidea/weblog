7d6778579560d939afc7798210f13325d3cb4280
Withnoidea <2482454653@qq.com>
2026-04-21 13:53:47 +0800

    自定义工具响应类

--- weblog-springboot/weblog-web/src/main/java/com/quanxiaoha/weblog/web/comtroller/TestController.java
+++ weblog-springboot/weblog-web/src/main/java/com/quanxiaoha/weblog/web/comtroller/TestController.java
@@ -1,6 +1,7 @@
 package com.quanxiaoha.weblog.web.comtroller;
 
 import com.quanxiaoha.weblog.common.aspect.ApiOperationLog;
+import com.quanxiaoha.weblog.common.utils.Response;
 import com.quanxiaoha.weblog.web.model.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -25,20 +26,20 @@ public class TestController {
 
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
-    public ResponseEntity<String> test(@RequestBody @Validated User user, BindingResult bindingResult) {
+    public Response test(@RequestBody @Validated User user, BindingResult bindingResult) {
         // 是否存在校验错误
         if (bindingResult.hasErrors()) {
             // 获取校验不通过字段的提示信息
-            String errorMsg = bindingResult.getFieldErrors()
-                    .stream()
-                    .map(FieldError::getDefaultMessage)
-                    .collect(Collectors.joining(", "));
+            String errorMsg = bindingResult.getFieldErrors()//获取所有校验失败的集合
+                    .stream()//把list变成流水线
+                    .map(error-> error.getDefaultMessage())
+                    .collect(Collectors.joining(", "));//结束流水线，把所有文字用逗号拼接成一个字符串
 
-            return ResponseEntity.badRequest().body(errorMsg);
+            return Response.fail(errorMsg);
         }
 
         // 返参
-        return ResponseEntity.ok("参数没有任何问题");
+        return Response.success();
     }
 
 }
