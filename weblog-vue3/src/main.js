2558696c8fc83eb73584d5ef920786f8d17604c6
Withnoidea <2482454653@qq.com>
2026-04-22 13:18:45 +0800

    登录页设计：支持响应式布局

--- weblog-vue3/src/main.js
+++ weblog-vue3/src/main.js
@@ -1,12 +1,20 @@
-import '@/assets/main.css'
+import "@/assets/main.css";
 
-import { createApp } from 'vue'
-import App from '@/App.vue'
+// 导入 Element Plus 图标
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+
+import { createApp } from "vue";
+import App from "@/App.vue";
 // 导入路由
-import router from '@/router'
+import router from "@/router";
 
-const app = createApp(App)
+const app = createApp(App);
+
+// 引入图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
 
 // 应用路由
-app.use(router)
-app.mount('#app')
+app.use(router);
+app.mount("#app");
