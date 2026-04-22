b95019540fd85f6eeb7066168affd7a53d2afd91
Withnoidea <2482454653@qq.com>
2026-04-22 14:15:12 +0800

    整合 Spring Security

---
+++ weblog-springboot/weblog-module-admin/src/main/java/com/quanxiaoha/weblog/admin/config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.quanxiaoha.weblog.admin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Author: Withnoidea
+ * @Date: 2026/4/22 14:09
+ * @Version: v1.0.0
+ * @Description: Spring Security 配置类
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter  {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .mvcMatchers("/admin/**").authenticated() // 认证所有以 /admin 为前缀的 URL 资源
+                .anyRequest().permitAll().and() // 其他都需要放行，无需认证
+                .formLogin().and() // 使用表单登录
+                .httpBasic(); // 使用 HTTP Basic 认证
+    }
+}
