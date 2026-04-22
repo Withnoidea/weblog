5d6c8e10b68fdf130853c462db2b62b30bd9f046
Withnoidea <2482454653@qq.com>
2026-04-22 09:46:23 +0800

    整合 Knife4j

--- weblog-springboot/weblog-web/src/main/java/com/quanxiaoha/weblog/web/controller/TestController.java
+++ weblog-springboot/weblog-web/src/main/java/com/quanxiaoha/weblog/web/controller/TestController.java
@@ -1,20 +1,15 @@
-package com.quanxiaoha.weblog.web.comtroller;
+package com.quanxiaoha.weblog.web.controller;
 
 import com.quanxiaoha.weblog.common.aspect.ApiOperationLog;
-import com.quanxiaoha.weblog.common.enums.ResponseCodeEnum;
-import com.quanxiaoha.weblog.common.exception.BizException;
 import com.quanxiaoha.weblog.common.utils.Response;
 import com.quanxiaoha.weblog.web.model.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.stream.Collectors;
 
 /**
  * @Author: Withnoidea
@@ -24,13 +19,15 @@ import java.util.stream.Collectors;
  */
 @RestController
 @Slf4j
+@Api(tags = "首页模块")
 public class TestController {
 
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
+    @ApiOperation(value = "测试接口")
     public Response test(@RequestBody @Validated User user) {
         return Response.success();
     }
-    
+
 
 }
